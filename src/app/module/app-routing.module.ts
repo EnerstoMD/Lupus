df77b61adcfbf471e179e214304604e167599f0a
hagaAR <haga.radaody@gmail.com>
2022-04-26 07:55:57 +0200

    reorg component/services folders 2

---
+++ src/app/module/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PatientsComponent } from '../patients/patients.component';
+import { ConsultationsComponent } from '../consultations/consultations.component';
+import { PatientfileComponent } from '../patientfile/patientfile.component';
+import { CalendarComponent } from '../calendar/calendar.component';
+import { HomeComponent } from '../home/home.component';
+import { AuthGuard } from '../auth/auth.guard';
+import { LoginComponent } from '../login/login.component';
+
+const routes: Routes = [
+  { path: '', component:HomeComponent},
+  { path: 'login', component:LoginComponent},
+  { path: 'patients', component: PatientsComponent,canActivate:[AuthGuard]},
+  { path: 'patients/:id', component: PatientfileComponent,canActivate:[AuthGuard]},
+  { path: 'consultations', component: ConsultationsComponent,canActivate:[AuthGuard]},
+  { path: 'calendar', component: CalendarComponent,canActivate:[AuthGuard]},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
